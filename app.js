46793a11bfd09e0eb79811bcde0e215f8bdd8d56
Ammar Azam Khan <khan.ammar6k@gmail.com>
2023-10-24 17:47:05 +0530

    added img

---
+++ app.js
@@ -0,0 +1,27 @@
+const openModal = document.querySelector('.open-modal');
+const modalContainer = document.querySelector('.modal-container');
+const closeBtn = document.querySelectorAll('.close-btn');
+
+openModal.addEventListener('click', function () {
+  modalContainer.style.display = 'block';
+});
+
+for (let btn of closeBtn) {
+  btn.addEventListener('click', function (e) {
+    modalContainer.style.display = 'none';
+    console.log(e);
+  });
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    modalContainer.style.display = 'none';
+  }
+});
+
+document.addEventListener('click', function (e) {
+  if (e.target.tagName === 'HTML') {
+    modalContainer.style.display = 'none';
+  }
+  console.log(e);
+});
